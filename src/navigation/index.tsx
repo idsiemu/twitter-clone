d3adba41a8039d51613c1a8f30c093bf5dc39af9
SMK <idsiemu@gmail.com>
2021-01-20 22:39:56 +0900

    commit

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -2,19 +2,58 @@ import React, { FC, useEffect } from 'react'
 import {NavigationContainer} from '@react-navigation/native';
 import AppStack from './appStack';
 import AuthStack from './authStack';
-import { authService } from '~/constants/firebase'
+import {useDispatch, useSelector} from 'react-redux';
+import {RootState} from '~/reducers';
+import auth from '@react-native-firebase/auth';
+import { setLoading, setUser } from '~/actions/user';
+import { Text, View, StyleSheet} from 'react-native';
 
+
+const styles = StyleSheet.create({
+    loading :{
+        flex:1,
+        alignItems:'center',
+        justifyContent:'center'
+    },
+});
 
 const Navigation : FC = () => {
+    const dispatch = useDispatch();
+    const {loading, user} = useSelector(
+        (state: RootState) => state.user,
+    );
     useEffect(() => {
-        authService.onAuthStateChanged((user) => {
-            // console.log(user)
+        auth().onAuthStateChanged((user) => {
+            if(user){
+                dispatch(setUser(
+                    {
+                        uid: user.uid,
+                        displayName: user.displayName,
+                        updateProfile: (args: object) => user.updateProfile(args),
+                    }
+                ))
+            }
+            if(loading){
+                dispatch(setLoading(false))
+            }
         })
     }, [])
     return (
-        <NavigationContainer>
-            <AuthStack />
-        </NavigationContainer>
+        <>
+            {loading ? 
+                <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
+                    <Text style={{fontSize:30}}>loading...</Text>
+                </View>
+            : 
+                <NavigationContainer>
+                    {user ? 
+                        <AppStack />
+                    :
+                        <AuthStack />
+                    }
+                </NavigationContainer>
+            }
+        </>
     )
 }
 
