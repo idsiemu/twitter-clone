74ed66932d139eee16a75b59912532f42b26e1e8
smk <idsiemu@gmail.com>
2021-02-01 18:48:08 +0900

    test2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import { getStatusBarHeight } from 'react-native-status-bar-height';
-import styled from 'styled-components/native';
 
 const Header = () => {
     return (
